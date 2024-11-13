7d7eb1ab65d4156de671c173fb8dc5040ce414e6
Mehak <mehakmatharu@Mehaks-MacBook-Air.local>
2024-11-13 14:20:41 -0800

    Order Screen

--- src/main/java/OrderScreen.java
+++ src/main/java/OrderScreen.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class OrderScreen {
     //All entries should show the newest entries first
     //o 1) Add Sandwich
@@ -5,4 +7,35 @@ public class OrderScreen {
     //o 3) Add Chips
     //o 4) Checkout
     //o 0) Cancel Order - delete the order and go back to the home page
+public static void orderScreen (){
+    System.out.println("(1)Add Sandwich \n(2) Add Drink \n (3)Add Chips \n (4)Checkout \n (5) Cancel Order");
+    do {
+        // String answer = getAnswer("--> "); //call getanswer method and put in arrow to type,
+        switch (getAnswer("--> ")){
+            case "1":
+                System.out.println("Add Sandwich"); //new order
+                break;
+            case "2":
+                System.out.println("Add Drink"); //new order
+                break;
+            case "3":
+                System.out.println("Add Chips"); //new order
+                break;
+            case "4":
+                System.out.println("Add Checkout"); //new order
+                break;
+            case "5":
+                System.out.println("Add Cancel Order"); //new order
+                System.exit(0); //exits program completely
+                break;
+            default:
+                System.out.println("Invalid option - Try Again");
+        }
+    } while(true);
+}
+    public static String getAnswer(String q){ //getAnswer method returns string argument and is the question pass in question and it returns the string user types
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(q);
+        return scanner.nextLine();
+    }
 }
