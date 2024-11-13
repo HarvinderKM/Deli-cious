4f4c9c686db25d5f46f9f51c01b4d372bf01ef41
Mehak <mehakmatharu@Mehaks-MacBook-Air.local>
2024-11-13 09:50:58 -0800

    Main Screen

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+import java.util.Scanner;
 
 public class MAIN {
-}
+    private Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        MAIN app = new MAIN();
+        app.run();
+    }
+
+    //method for displaying main menu
+    public void displayMainMenu(){
+    }
+
+    // create method for keeping the system running
+    public void run() {
+        boolean running = true; //var will control the loop
+        while (running) { // loop continues until running is set to false
+            displayMainMenu(); //create a display main menu method  - this will display the main menu to the user every time it loops
+
+            int choice = scanner.nextInt();// users menu choice
+            scanner.nextLine(); //clears extra characters
+
+            switch (choice){
+                case 1:
+                    //code
+                    break;
+                case 2:
+                    break;
+                default:
+                    //code
+            }
+
+    //method for starting new order
+    public void startNewOrder(){
+
+            }
+
+    }
+}}
